5ff9fda388f054dae4594a230dd90558afa323b6
yemmyharry <yemmyharry@gmail.com>
2023-03-29 08:34:16 +0100

    Added cors middleware to route method

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -1,7 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"net/http"
+)
 
 func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
 
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	return mux
 }
